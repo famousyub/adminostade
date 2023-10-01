185534682ddff5b3bd135df4ba4ebf9685837112
famousyub <famousmeyub@gmail.com>
2023-10-01 01:07:28 +0100

    yujb

---
+++ src/app/model/product.ts
@@ -0,0 +1,14 @@
+export class Product {
+
+
+
+  id: number;
+  title: string;
+  pricehour: number;
+  imageURL: string;
+  pathURL: string;
+  address: string;
+  description: string;
+  startProgram: string;
+  endProgram: string;
+}
---
+++ src/app/services/admin.service.ts
@@ -0,0 +1,85 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {User} from '../model/user';
+import {Product} from '../model/product';
+import {Transaction} from '../model/transaction';
+
+let API_URL = "http://localhost:8081/api/admin/";
+let API_URL2 = "http://localhost:8081/";
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  currentUser: User;
+  headers: HttpHeaders;
+
+  constructor(private http: HttpClient) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.headers = new HttpHeaders({
+      authorization:'Bearer ' + this.currentUser.token,
+      "Content-Type":"application/json; charset=UTF-8"
+    });
+  }
+
+  updateUser(user: User): Observable<any> {
+    return this.http.put(API_URL + "user-update", JSON.stringify(user),
+  {headers: this.headers});
+  }
+
+  deleteUser(user: User): Observable<any> {
+    return this.http.post(API_URL + "user-delete", JSON.stringify(user),
+  {headers: this.headers});
+  }
+
+  findAllUsers(): Observable<any> {
+    return this.http.get(API_URL + "user-all",
+  {headers: this.headers});
+  }
+
+  getCustomerReservations(customerId: number) {
+    return this.http.get<any[]>(API_URL2 + 'api/customer/getCustomerReservations/' + customerId);
+  }
+  numberOfUsers(): Observable<any> {
+    return this.http.get(API_URL + "user-number",
+  {headers: this.headers});
+  }
+
+  //products
+  createProduct(product: Product): Observable<any> {
+    return this.http.post(API_URL + "product-create", JSON.stringify(product),
+  {headers: this.headers});
+  }
+
+  updateProduct(product: Product): Observable<any> {
+    return this.http.put(API_URL + "product-update", JSON.stringify(product),
+  {headers: this.headers});
+  }
+
+  deleteProduct(product: Product): Observable<any> {
+    return this.http.post(API_URL + "product-delete", JSON.stringify(product),
+  {headers: this.headers});
+  }
+
+  findAllProducts(): Observable<any> {
+    return this.http.get(API_URL + "product-all",
+  {headers: this.headers});
+  }
+
+  numberOfProducts(): Observable<any> {
+    return this.http.get(API_URL + "product-number",
+  {headers: this.headers});
+  }
+
+  //transactions
+  findAllTransactions(): Observable<any> {
+    return this.http.get(API_URL + "transaction-all",
+   {headers: this.headers});
+  }
+
+  numberOfTransactions(): Observable<any> {
+    return this.http.get(API_URL + "transaction-number",
+  {headers: this.headers});
+  }
+}
